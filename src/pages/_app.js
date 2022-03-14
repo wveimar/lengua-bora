3b35a400c107361f03766bce8749cbef33c88fce
wveimar.mamian@gmail.com <wveimar.mamian@gmail.com>
2022-03-14 12:40:26 -0500

    configuracion de useAuth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
 import "@styles/tailwind.css";
-import MainLayout from "@layout/MainLayout"
+import MainLayout from "@layout/MainLayout";
+import {ProviderAuth} from "hooks/useAuth"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <ProviderAuth>
     <MainLayout>
     <Component {...pageProps} />
     </MainLayout>
+    </ProviderAuth>
     </>
   )
 }
